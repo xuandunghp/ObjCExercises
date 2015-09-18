7a72b0984273f16c3fff01de5ab2cc1bdb90165a
Dzung Tran <xuandung.hp.vn@gmail.com>
2015-09-18 23:45:30 +0700

    Initial Commit

---
+++ ObjCExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCExample
+//
+//  Created by Dzung Tran on 9/18/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCExample
+//
+//  Created by Dzung Tran on 9/18/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
